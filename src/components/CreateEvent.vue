c1d75d39359257af40959b94f587386ae2e4cfc4
Batmanpouknight <irfn7pouyan@gmail.com>
2024-08-03 01:02:37 -0700

    moved data to app.vue because that seems to be better cause it has to be passed to multiple coomponets, added the date to the add event screen

--- src/components/CreateEvent.vue
+++ src/components/CreateEvent.vue
@@ -1,11 +1,20 @@
 <script setup>
-import { ref } from 'vue'
-const counter = ref(0)
+// import { ref } from 'vue'
+const props = defineProps(['months', 'daySelected', 'currentMonth'])
 </script>
 
 <template>
-  <div>
-    <h1>Counter: {{ counter }}</h1>
-    <button @click="counter++">Increment</button>
-  </div>
+  Date: {{ months[currentMonth].name }} {{ daySelected }}
+  <form>
+    <label for="name-input">Name: </label>
+    <input type="text" class="name-input" />
+
+    <button type="submit">Add Event</button>
+  </form>
 </template>
+
+<style scoped>
+input {
+  width: 80%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Calender from './components/CalenderInterface.vue'
+import CreateEvent from './components/CreateEvent.vue'
 
-createApp(App).mount('#app')
+createApp(App).component('Calender', Calender).component('CreateEvent', CreateEvent).mount('#app')
