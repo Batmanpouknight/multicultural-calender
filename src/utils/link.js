6ae3e47f6db192fe3d4935721734c29076826902
Batmanpouknight <irfn7pouyan@gmail.com>
2024-09-24 23:25:59 -0700

    added the link to environment variable

--- src/utils/link.js
+++ src/utils/link.js
@@ -1,2 +1,3 @@
-export const link = 'https://calender-database.onrender.com'
+// export const link = 'https://calender-database.onrender.com'
 // export const link = 'http://localhost:3000'
+export const link = import.meta.env.VITE_API_URL
